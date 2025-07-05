e5a7fe1986950b61669786785f5ad62b678313c7
Su-Yong <simssy2205@gmail.com>
2025-07-05 19:53:53 +0900

    chore(primitives): add dts

--- packages/primitives/vite.config.ts
+++ packages/primitives/vite.config.ts
@@ -1,20 +1,27 @@
 import { defineConfig } from 'rolldown-vite';
 
 import solid from 'vite-plugin-solid';
+import dts from 'unplugin-dts/rolldown';
 
 export default defineConfig({
   plugins: [
     solid(),
+    dts({ bundleTypes: true }),
   ],
   build: {
     outDir: 'dist',
     lib: {
       entry: 'src/index.ts',
-      name: 'Primitives',
-      fileName: (format) => `suis.primitives.${format}.js`,
+      name: 'suis-primitives',
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ['solid-js'],
+      output: {
+        globals: {
+          'solid-js': 'solid-js',
+        },
+      },
     },
   },
 });
