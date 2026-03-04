9b2aa7d63d4c2b0e8d83f497e8bda2c37f5e94b9
Su-Yong <simssy2205@gmail.com>
2026-03-04 14:45:09 +0900

    feat(ui): support custom indicator rendering for `CheckBox`

--- packages/ui/src/ui/CheckBox/CheckBox.tsx
+++ packages/ui/src/ui/CheckBox/CheckBox.tsx
@@ -1,4 +1,5 @@
-import { splitProps, ValidComponent } from 'solid-js';
+import { JSX, mergeProps, splitProps, ValidComponent } from 'solid-js';
+import { Dynamic } from 'solid-js/web';
 import {
   CheckBox as BaseCheckBox,
   CheckBoxLabel as BaseCheckBoxLabel,
@@ -16,15 +17,23 @@ type CheckBoxOnlyProps = {
 
   inputProps?: BaseCheckBoxIndicatorProps;
   labelProps?: BaseCheckBoxLabelProps;
+
+  renderIndicator?: (props: CheckBoxIndicatorProps) => JSX.Element;
 }
 export type CheckBoxProps<T extends ValidComponent = 'input'> =
   Omit<BaseCheckBoxProps<T>, keyof CheckBoxOnlyProps>
   & CheckBoxOnlyProps;
 export const CheckBox = <T extends ValidComponent = 'input'>(props: CheckBoxProps<T>) => {
-  const [local, elementProps, rest] = splitProps(
-    props,
+  const [local, elementProps, renderer, rest] = splitProps(
+    mergeProps(
+      {
+        renderIndicator: DefaultCheckBoxIndicator,
+      },
+      props,
+    ),
     ['checked', 'onChecked'],
-    ['inputProps', 'labelProps']
+    ['inputProps', 'labelProps'],
+    ['renderIndicator'],
   );
 
   return (
@@ -49,25 +58,35 @@ export const CheckBox = <T extends ValidComponent = 'input'>(props: CheckBoxProp
             ...elementProps.inputProps?.classList
           }}
         >
-          <div class={indicatorStyle}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class={checkStyle}
-            >
-              <path d="M20 6 9 17l-5-5"/>
-            </svg>
-          </div>
+          <Dynamic<(props: CheckBoxIndicatorProps) => JSX.Element>
+            checked={local.checked}
+            component={renderer.renderIndicator}
+          />
         </BaseCheckBoxIndicator>
         {props.name}
       </BaseCheckBoxLabel>
     </BaseCheckBox>
   );
 };
+
+export type CheckBoxIndicatorProps = {
+  checked?: boolean;
+};
+const DefaultCheckBoxIndicator = (_: CheckBoxIndicatorProps) => (
+  <div class={indicatorStyle}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class={checkStyle}
+    >
+      <path d="M20 6 9 17l-5-5" />
+    </svg>
+  </div>
+);
